383a1fff0d3edb818d71d5b88e6686169c17deb7
asksaito <es.engineer79@gmail.com>
2022-03-27 23:43:24 +0900

    Refactor code

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,13 +2,11 @@ package com.example;
 
 import com.example.file_io_library.FileIoLibrary;
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String args[]) throws Exception {
         String src = "E:\\temp\\gradle_test\\apex.txt";
         String dest = "E:\\temp\\gradle_test\\out.txt";
 
-        FileIoLibrary.sample(src, dest);
+        FileIoLibrary.jdk1_0Style(src, dest);
     }
 }
--- src/main/java/com/example/file_io_library/FileIoLibrary.java
+++ src/main/java/com/example/file_io_library/FileIoLibrary.java
@@ -3,28 +3,27 @@ package com.example.file_io_library;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FileIoLibrary {
-    public static void sample(String srcPath, String destPath) throws IOException {
+    public static void jdk1_0Style(String srcPath, String destPath) throws IOException {
         File srcFile = new File(srcPath);
         File destFile = new File(destPath);
 
         BufferedReader reader = null;
-        BufferedOutputStream writer = null;
+        OutputStream writer = null;
         try {
             reader = new BufferedReader(new InputStreamReader(new FileInputStream(srcFile), StandardCharsets.UTF_8));
             writer = new BufferedOutputStream(new FileOutputStream(destFile));
 
-            String str;
-            while ((str = reader.readLine()) != null) {
-                str += "\r\n";
-                byte[] binary = str.getBytes(Charset.forName("SJIS"));
+            String line;
+            while ((line = reader.readLine()) != null) { // 1行読み出し
+                // プラットフォームに応じた改行文字を付加
+                line += System.getProperty("line.separator");
+                // Shift-JIS binary 取得
+                byte[] binary = line.getBytes(Charset.forName("SJIS"));
+                // ファイルに書き込み
                 writer.write(binary, 0, binary.length);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         } finally {
             if (writer != null) {
                 writer.close();
