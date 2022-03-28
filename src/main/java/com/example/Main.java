0861c18539f5adf93d6b3052c4ace6c1fbd2e5f5
asksaito <es.engineer79@gmail.com>
2022-03-29 00:39:16 +0900

    no message

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,49 @@
 package com.example;
 
 import com.example.file_io_library.FileIoLibrary;
+import org.apache.commons.lang3.time.StopWatch;
 
 public class Main {
     public static void main(String args[]) throws Exception {
-        String src = "E:\\temp\\gradle_test\\apex.txt";
-        String dest = "E:\\temp\\gradle_test\\out.txt";
+//        String src = "E:\\temp\\gradle_test\\apex.txt";
+//        String dest = "E:\\temp\\gradle_test\\";
+        String src = "D:\\temp\\test\\sample.csv";
+        String dest = "D:\\temp\\test\\";
 
-        // test 2
-        FileIoLibrary.jdk1_0Style(src, dest);
+        StopWatch stopWatch = new StopWatch();
+
+//        // test JDK 1.0
+//        stopWatch.start();
+//        FileIoLibrary.jdk1_0Style(src, dest + "jdk10.txt");
+//        stopWatch.stop();
+//        System.out.println("JDK 1.0: " + stopWatch.getTime() + " (msec)");
+//
+//        // test JDK 1.7
+//        stopWatch.reset();
+//        stopWatch.start();
+//        FileIoLibrary.jdk1_7Style(src, dest + "jdk17.txt");
+//        stopWatch.stop();
+//        System.out.println("JDK 1.7: " + stopWatch.getTime() + " (msec)");
+//
+//        // test JDK 1.8
+//        stopWatch.reset();
+//        stopWatch.start();
+//        FileIoLibrary.jdk1_8Style(src, dest + "jdk18.txt");
+//        stopWatch.stop();
+//        System.out.println("JDK 1.8: " + stopWatch.getTime() + " (msec)");
+
+        // test apache
+        stopWatch.reset();
+        stopWatch.start();
+        FileIoLibrary.apacheCommonsStyle(src, dest + "apache.txt");
+        stopWatch.stop();
+        System.out.println("Apache: " + stopWatch.getTime() + " (msec)");
+
+//        // test guava
+//        stopWatch.reset();
+//        stopWatch.start();
+//        FileIoLibrary.googleGuavaStyle(src, dest + "guava.txt");
+//        stopWatch.stop();
+//        System.out.println("Guava: " + stopWatch.getTime() + " (msec)");
     }
 }
--- src/main/java/com/example/file_io_library/FileIoLibrary.java
+++ src/main/java/com/example/file_io_library/FileIoLibrary.java
@@ -1,8 +1,14 @@
 package com.example.file_io_library;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileIoLibrary {
     public static void jdk1_0Style(String srcPath, String destPath) throws IOException {
@@ -30,6 +36,88 @@ public class FileIoLibrary {
             }
             if (reader != null) {
                 reader.close();
+            }
+        }
+    }
+
+    public static void jdk1_7Style(String srcPath, String destPath) throws IOException {
+        Path srcFile = Paths.get(srcPath);
+        Path destFile = Paths.get(destPath);
+
+        try (BufferedReader br = Files.newBufferedReader(srcFile, StandardCharsets.UTF_8)) {
+            try (BufferedWriter bw = Files.newBufferedWriter(destFile, Charset.forName("SJIS"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    // ファイルに書き込み
+                    bw.write(line + System.lineSeparator());
+                }
+            }
+        }
+    }
+
+    public static void jdk1_8Style(String srcPath, String destPath) throws IOException {
+        Path srcFile = Paths.get(srcPath);
+        Path destFile = Paths.get(destPath);
+
+        try (BufferedReader br = Files.newBufferedReader(srcFile, StandardCharsets.UTF_8)) {
+            try (BufferedWriter bw = Files.newBufferedWriter(destFile, Charset.forName("SJIS"))) {
+                br.lines()
+                        .map(s -> s + System.lineSeparator())
+//                        .forEach(bw::write) // 例外が出なければこの書き方が良いのだが
+                        .forEach(s -> {
+                            try {
+                                // ファイルに書き込み
+                                bw.write(s);
+                            } catch (IOException e) {
+                                // 例外処理をしないといけないのがイマイチ
+                                e.printStackTrace();
+                            }
+                        });
+            }
+        }
+    }
+
+    public static void apacheCommonsStyle(String srcPath, String destPath) throws IOException {
+        File srcFile = new File(srcPath);
+        File destFile = new File(destPath);
+
+        // LineIterator を使う場合
+        try (LineIterator it = FileUtils.lineIterator(srcFile, "UTF-8")) {
+            try (FileOutputStream out = FileUtils.openOutputStream(destFile)) {
+                while (it.hasNext()) {
+                    String line = it.nextLine() + System.lineSeparator();
+                    // ファイルに書き込み
+                    out.write(line.getBytes(Charset.forName("SJIS")));
+
+                    // FileUtils.write(destFile, line, "SJIS", true); // 毎回ファイルオープンするので非常に低速
+                }
+            }
+        }
+
+        // BufferedReader / BufferedOutputStream を使う場合 (apache commons)
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(FileUtils.openInputStream(srcFile), StandardCharsets.UTF_8))) {
+            try (BufferedOutputStream out = new BufferedOutputStream(FileUtils.openOutputStream(destFile))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    line += System.lineSeparator();
+                    // ファイルに書き込み
+                    out.write(line.getBytes(Charset.forName("SJIS")));
+                }
+            }
+        }
+    }
+
+    public static void googleGuavaStyle(String srcPath, String destPath) throws IOException {
+        File srcFile = new File(srcPath);
+        File destFile = new File(destPath);
+
+        try (BufferedReader br = com.google.common.io.Files.newReader(srcFile, StandardCharsets.UTF_8)) {
+            try (BufferedWriter bw = com.google.common.io.Files.newWriter(destFile, Charset.forName("SJIS"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    // ファイルに書き込み
+                    bw.write(line + System.lineSeparator());
+                }
             }
         }
     }
